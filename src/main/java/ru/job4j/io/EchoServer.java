7b20acefdaaa6217b7a6468dba2571a46f4bb9a9
ErmolaevVlad <ermolaev.vlad.0109@gmail.com>
2025-02-06 10:58:03 +0700

    3. Бот [#7921 #531415]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -13,18 +13,17 @@ public class EchoServer {
                      BufferedReader input = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    for (String string = input.readLine(); string != null && !string.isEmpty(); string = input.readLine()) {
-                        System.out.println(string);
-                        if (string.contains("msg=")) {
-                            String msg = string.split("msg=")[1].split(" ")[0];
-                            if ("Hello".equals(msg)) {
-                                output.write("Hello\r\n\r\n".getBytes());
-                            } else if ("Exit".equals(msg)) {
-                                server.close();
-                                output.write("server shut down\r\n\r\n".getBytes());
-                            } else {
-                                output.write("What\r\n\r\n".getBytes());
-                            }
+                    String string = input.readLine();
+                    System.out.println(string);
+                    if (string != null && string.contains("msg=")) {
+                        String msg = string.split("msg=")[1].split(" ")[0];
+                        if ("Hello".equals(msg)) {
+                            output.write("Hello\r\n\r\n".getBytes());
+                        } else if ("Exit".equals(msg)) {
+                            server.close();
+                            output.write("server shut down\r\n\r\n".getBytes());
+                        } else {
+                            output.write("What\r\n\r\n".getBytes());
                         }
                     }
                     output.flush();
