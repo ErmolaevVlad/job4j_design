65d740259e4ee7e6ccfa94cfff9e41ab92a511a9
ErmolaevVlad <ermolaev.vlad.0109@gmail.com>
2025-05-06 15:45:02 +0700

    5. Курсор в SQL [#504816]

---
+++ db/ObjectsDB/cursor_in_sql.sql
@@ -0,0 +1,11 @@
+begin transaction;
+declare cursor_p scroll cursor for select * from products;
+fetch last from cursor_p;
+move backward 4 from cursor_p;
+fetch prior from cursor_p;
+move backward 7 from cursor_p;
+fetch prior from cursor_p;
+move backward 4 from cursor_p;
+fetch prior from cursor_p;
+fetch prior from cursor_p;
+commit;
