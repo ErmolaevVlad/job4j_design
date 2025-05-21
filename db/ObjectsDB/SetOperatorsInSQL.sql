38a36bab81a39613ed25f46f95e28c466b5e61ff
ErmolaevVlad <ermolaev.vlad.0109@gmail.com>
2025-05-21 17:02:19 +0700

    10. Операторы множества в SQL [#504896]

---
+++ db/ObjectsDB/SetOperatorsInSQL.sql
@@ -0,0 +1,44 @@
+CREATE TABLE movie
+(
+    id       SERIAL PRIMARY KEY,
+    name     text,
+    director text
+);
+
+CREATE TABLE book
+(
+    id     SERIAL PRIMARY KEY,
+    title  text,
+    author text
+);
+
+INSERT INTO movie (name, director)
+VALUES ('Марсианин', 'Ридли Скотт'),
+       ('Матрица', 'Братья Вачовски'),
+       ('Властелин колец', 'Питер Джексон'),
+       ('Гарри Поттер и узник Азкабана', 'Альфонсо Куарон'),
+       ('Железный человек', 'Джон Фавро');
+
+INSERT INTO book (title, author)
+VALUES ('Гарри Поттер и узник Азкабана', 'Джоан Роулинг'),
+       ('Властелин колец', 'Джон Толкин'),
+       ('1984', 'Джордж Оруэлл'),
+       ('Марсианин', 'Энди Уир'),
+       ('Божественная комедия', 'Данте Алигьери');
+
+
+select name from movie
+intersect
+select title from book;
+
+select title from book
+except
+select name from movie;
+
+select m.name, b.title from movie m
+left join book b on m.name = b.title
+where b.title is null
+union
+select m.name, b.title from movie m
+right join book b on m.name = b.title
+where m.name is null;
